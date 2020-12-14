70b4c27020d7e0cbf17c5921fbe801ba11060f13
wangyuxin <wangyuxin@mokahr.com>
2020-12-14 17:54:00 +0800

    [FIX] fix bug

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-import sys
 import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
@@ -10,42 +9,15 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
-def get_conda_env_name():
-    python_version = "{{cookiecutter.python_version}}"
-    version = ""
-    for i in python_version:
-        if i in "0123456789":
-            version += i
-    return "py" + version[:2]
+# def get_conda_python():
+#     conda_env_path = "{{cookiecutter.conda_env_path}}"
+#     return conda_env_path + "/bin/python"
 
 
 if __name__ == "__main__":
-    try:
-        from conda.core.envs_manager import list_all_known_prefixes
-    except ImportError:
-        print("========= ERROR =========")
-        print("install miniconda for managing python version")
-        print("========= ERROR =========")
-        sys.exit(1)
-
     if "n" in "{{ cookiecutter.command_line_interface|lower}}":
         cli_file = os.path.join("{{ cookiecutter.module_name }}", "cli.py")
         remove_file(cli_file)
 
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
-
-    target_env_name = get_conda_env_name()
-    for n in list_all_known_prefixes():
-        env_name = n.rsplit("/", 1)[-1]
-        if env_name == target_env_name:
-            subprocess.run(["poetry", "env", "use", f"{n}/bin/python"])
-            subprocess.run(["poetry", "install"])
-            break
-    else:
-        print("========= ERROR =========")
-        print(
-            f"Can not find matched conda env, install it, use:\nconda create -n {target_env_name} python={target_env_name[-2]}.{target_env_name[-1]}"
-        )
-        print("========= ERROR =========")
-        sys.exit(1)
--- ui.py
+++ ui.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
+import re
+import subprocess
 from pathlib import Path
 
 import streamlit as st
 from cookiecutter.generate import generate_files
+from conda.base.context import context
+from conda.core.envs_manager import list_all_known_prefixes
+
+
+def get_python_version_from_env_path(env_path):
+    binary_path = str(Path(env_path) / "bin" / "python")
+    sub_result = subprocess.run([binary_path, "--version"], capture_output=True)
+    output = sub_result.stdout.decode("utf-8") + sub_result.stderr.decode("utf-8")
+    finds = re.findall(r'[Pp]ython\s?([23]\.\d{1,2})\.\d{1,2}', output)
+    if finds:
+        return finds[0]
+    else:
+        raise Exception("不能正确的获得 python 版本")
 
 
 st.subheader("Meta info")
@@ -12,33 +27,46 @@ email = col2.text_input("email", "yuxin_wang_94@163.com")
 st.subheader("Project info")
 col21, col22, col23 = st.beta_columns(3)
 project_name = col21.text_input("project_name", "Python Project Name")
-project_slug = col22.text_input("project_slug", project_name.lower().replace(' ', '_').replace('-', '_'))
-module_name = col23.text_input("module_name", project_slug)
+module_name = col22.text_input("module_name", project_name.lower().replace(' ', '_').replace('-', '_'))
+open_source_license = col23.selectbox("license", ["Not open source", "MIT license", "BSD license", "ISC license", "Apache Software License 2.0", "GNU General Public License v3"])
 project_short_description = st.text_area("project_short_description", "description...")
-col31, col32 = st.beta_columns(2)
-python_version = col31.text_input("python_version", "^3.6.8")
-open_source_license = col32.selectbox("license", ["Not open source", "MIT license", "BSD license", "ISC license", "Apache Software License 2.0", "GNU General Public License v3"])
 st.subheader("Extra Options")
 command_line_interface = st.checkbox("Command Line Interface")
 
+st.subheader("Choose Virtual Envs")
+conda_env_paths = list_all_known_prefixes()
+use_new = st.checkbox("Use New Virtual Environment", False)
+if use_new:
+    col41, col42 = st.beta_columns(2)
+    with col41:
+        new_env_name = st.text_input("Env Name")
+    with col42:
+        python_version = st.selectbox("Python Version", ["2.7", "3.6", "3.7", "3.8"], index=1)
+    conda_env_path = str(Path(context.envs_dirs[0]) / new_env_name)
+else:
+    conda_env_path = st.selectbox("Conda Envs", conda_env_paths)
+    python_version = get_python_version_from_env_path(conda_env_path)
+
+st.subheader("Preview")
 info = {
     "full_name": full_name,
     "email": email,
     "project_name": project_name,
     "project_short_description": project_short_description,
-    "project_slug": project_slug,
+    "project_slug": module_name,
     "module_name": module_name,
-    "python_version": python_version,
+    "conda_env_path": conda_env_path,
+    "python_version": "^" + python_version,
     "command_line_interface": command_line_interface,
     "open_source_license": open_source_license
 }
-
-st.subheader("Preview")
 st.write(info)
 
 st.subheader("Create Project")
 project_root_dir = st.text_input("输入 Project Root Dir:", str(Path.home().resolve() / "workspace"))
 if st.button("Create"):
+    if use_new:
+        subprocess.run(["conda", "create", "-n", new_env_name, f"python={python_version}"])
     context = {"cookiecutter": info}
     kagebu_dir = Path(".").resolve()
     context['cookiecutter']['_template'] = kagebu_dir
